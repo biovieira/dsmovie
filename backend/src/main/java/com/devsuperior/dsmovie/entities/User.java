7bf30bee44c66fb5f23226393ec4ae163b709051
Fabiano Vieira <vieira.bio@gmail.com>
2022-01-13 20:22:29 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String Email;
+	public User(Long id, String email) {
+		
+		this.id = id;
+		Email = email;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	
+	
+
+}
